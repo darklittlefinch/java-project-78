842c05855b9f84e8f9fcdcc2acaa8e5a36d45212
Eliza Moritz <dark.little.finch@gmail.com>
2023-08-04 22:32:52 +0700

    feat: debug print

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -1,7 +1,37 @@
 package hexlet.code;
 
+import hexlet.code.schemas.StringSchema;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Validator validator = new Validator();
+        StringSchema stringSchema = validator.string();
+
+        System.out.println("DEFAULT CHECK");
+        System.out.println(stringSchema.isValid(null));
+        System.out.println(stringSchema.isValid(""));
+        System.out.println(stringSchema.isValid("fox"));
+        System.out.println(stringSchema.isValid("what does the fox say"));
+
+        System.out.println("\nREQUIRED CHECK");
+        stringSchema.required();
+        System.out.println(stringSchema.isValid(null));
+        System.out.println(stringSchema.isValid(""));
+        System.out.println(stringSchema.isValid("fox"));
+        System.out.println(stringSchema.isValid("what does the fox say"));
+
+        System.out.println("\nMIN LENGTH CHECK");
+        stringSchema.minLength(5);
+        System.out.println(stringSchema.isValid(null));
+        System.out.println(stringSchema.isValid(""));
+        System.out.println(stringSchema.isValid("fox"));
+        System.out.println(stringSchema.isValid("what does the fox say"));
+
+        System.out.println("\nCONTENTS CHECK");
+        stringSchema.contains("what");
+        System.out.println(stringSchema.isValid(null));
+        System.out.println(stringSchema.isValid(""));
+        System.out.println(stringSchema.isValid("fox"));
+        System.out.println(stringSchema.isValid("what does the fox say"));
     }
 }
