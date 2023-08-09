7003ac31c437117ae9ef03b5f10e2e16ac732764
Eliza Moritz <dark.little.finch@gmail.com>
2023-08-09 16:52:08 +0700

    add new debug print

--- app/src/main/java/hexlet/code/Main.java
+++ app/src/main/java/hexlet/code/Main.java
@@ -1,13 +1,17 @@
 package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
+import hexlet.code.schemas.NumberSchema;
 
 public class Main {
     public static void main(String[] args) {
-        Validator validator = new Validator();
-        StringSchema stringSchema = validator.string();
 
-        System.out.println("DEFAULT CHECK");
+        // String schema test
+        System.out.println("STRINGS");
+        Validator validator1 = new Validator();
+        StringSchema stringSchema = validator1.string();
+
+        System.out.println("\nDEFAULT CHECK");
         System.out.println(stringSchema.isValid(null));
         System.out.println(stringSchema.isValid(""));
         System.out.println(stringSchema.isValid("fox"));
@@ -21,17 +25,49 @@ public class Main {
         System.out.println(stringSchema.isValid("what does the fox say"));
 
         System.out.println("\nMIN LENGTH CHECK");
-        stringSchema.minLength(5);
+        stringSchema.minLength(3);
         System.out.println(stringSchema.isValid(null));
         System.out.println(stringSchema.isValid(""));
         System.out.println(stringSchema.isValid("fox"));
         System.out.println(stringSchema.isValid("what does the fox say"));
 
         System.out.println("\nCONTENTS CHECK");
-        stringSchema.contains("what");
+        stringSchema.contains("fox");
         System.out.println(stringSchema.isValid(null));
         System.out.println(stringSchema.isValid(""));
         System.out.println(stringSchema.isValid("fox"));
         System.out.println(stringSchema.isValid("what does the fox say"));
+
+        // number schema test
+        System.out.println("\n\nNUMBERS");
+        Validator validator2 = new Validator();
+        NumberSchema numberSchema = validator2.number();
+
+        System.out.println("\nDEFAULT CHECK");
+        System.out.println(numberSchema.isValid(null));
+        System.out.println(numberSchema.isValid(10));
+        System.out.println(numberSchema.isValid(-10));
+        System.out.println(numberSchema.isValid("10"));
+
+        System.out.println("\nREQUIRED CHECK");
+        numberSchema.required();
+        System.out.println(numberSchema.isValid(null));
+        System.out.println(numberSchema.isValid(10));
+        System.out.println(numberSchema.isValid(-10));
+        System.out.println(numberSchema.isValid("10"));
+
+        System.out.println("\nPOSITIVE CHECK");
+        numberSchema.positive();
+        System.out.println(numberSchema.isValid(null));
+        System.out.println(numberSchema.isValid(10));
+        System.out.println(numberSchema.isValid(-10));
+        System.out.println(numberSchema.isValid("10"));
+
+        System.out.println("\nRANGE CHECK");
+        numberSchema.range(-10, 0);
+        System.out.println(numberSchema.isValid(null));
+        System.out.println(numberSchema.isValid(10));
+        System.out.println(numberSchema.isValid(-10));
+        System.out.println(numberSchema.isValid("10"));
     }
 }
