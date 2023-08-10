6d246d2fe2af29a561e3cd02e5ccba324f2fe4c0
Eliza Moritz <dark.little.finch@gmail.com>
2023-08-11 03:55:33 +0700

    style: tests were optimized

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -43,158 +43,158 @@ public class ValidatorTest {
     void stringSchemaTestDefault() {
         stringSchema = validator.string();
 
-        assertThat(stringSchema.isValid(null)).isEqualTo(true);
-        assertThat(stringSchema.isValid("")).isEqualTo(true);
-        assertThat(stringSchema.isValid("fox")).isEqualTo(true);
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(stringSchema.isValid(null)).isTrue();
+        assertThat(stringSchema.isValid("")).isTrue();
+        assertThat(stringSchema.isValid("fox")).isTrue();
+        assertThat(stringSchema.isValid("what does the fox say")).isTrue();
     }
 
     @Test
     void stringSchemaTestRequired() {
         stringSchema = validator.string().required();
 
-        assertThat(stringSchema.isValid(null)).isEqualTo(false);
-        assertThat(stringSchema.isValid("")).isEqualTo(false);
-        assertThat(stringSchema.isValid("fox")).isEqualTo(true);
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(stringSchema.isValid(null)).isFalse();
+        assertThat(stringSchema.isValid("")).isFalse();
+        assertThat(stringSchema.isValid("fox")).isTrue();
+        assertThat(stringSchema.isValid("what does the fox say")).isTrue();
     }
 
     @Test
     void stringSchemaTestMinLength() {
         stringSchema = validator.string().minLength(5);
 
-        assertThat(stringSchema.isValid(null)).isEqualTo(true);
-        assertThat(stringSchema.isValid("")).isEqualTo(false);
-        assertThat(stringSchema.isValid("fox")).isEqualTo(false);
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(stringSchema.isValid(null)).isTrue();
+        assertThat(stringSchema.isValid("")).isFalse();
+        assertThat(stringSchema.isValid("fox")).isFalse();
+        assertThat(stringSchema.isValid("what does the fox say")).isTrue();
 
         stringSchema.minLength(3);
 
-        assertThat(stringSchema.isValid("fox")).isEqualTo(true);
+        assertThat(stringSchema.isValid("fox")).isTrue();
     }
 
     @Test
     void stringSchemaTestContains() {
         stringSchema = validator.string().contains("fox");
 
-        assertThat(stringSchema.isValid(null)).isEqualTo(false);
-        assertThat(stringSchema.isValid("")).isEqualTo(false);
-        assertThat(stringSchema.isValid("fox")).isEqualTo(true);
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(stringSchema.isValid(null)).isFalse();
+        assertThat(stringSchema.isValid("")).isFalse();
+        assertThat(stringSchema.isValid("fox")).isTrue();
+        assertThat(stringSchema.isValid("what does the fox say")).isTrue();
 
         stringSchema.contains("what");
 
-        assertThat(stringSchema.isValid("fox")).isEqualTo(false);
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(stringSchema.isValid("fox")).isFalse();
+        assertThat(stringSchema.isValid("what does the fox say")).isTrue();
 
         stringSchema.contains("something else");
 
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(false);
+        assertThat(stringSchema.isValid("what does the fox say")).isFalse();
     }
 
     @Test
     void stringSchemaTestAllMethods() {
         stringSchema = validator.string().required().minLength(5);
 
-        assertThat(stringSchema.isValid(null)).isEqualTo(false);
-        assertThat(stringSchema.isValid("")).isEqualTo(false);
-        assertThat(stringSchema.isValid("fox")).isEqualTo(false);
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(stringSchema.isValid(null)).isFalse();
+        assertThat(stringSchema.isValid("")).isFalse();
+        assertThat(stringSchema.isValid("fox")).isFalse();
+        assertThat(stringSchema.isValid("what does the fox say")).isTrue();
 
         stringSchema.contains("fox");
 
-        assertThat(stringSchema.isValid("fox")).isEqualTo(false);
-        assertThat(stringSchema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(stringSchema.isValid("fox")).isFalse();
+        assertThat(stringSchema.isValid("what does the fox say")).isTrue();
     }
 
     @Test
     void numberSchemaTestDefault() {
         numberSchema = validator.number();
 
-        assertThat(numberSchema.isValid(null)).isEqualTo(true);
-        assertThat(numberSchema.isValid(10)).isEqualTo(true);
-        assertThat(numberSchema.isValid(-10)).isEqualTo(true);
-        assertThat(numberSchema.isValid("10")).isEqualTo(false);
+        assertThat(numberSchema.isValid(null)).isTrue();
+        assertThat(numberSchema.isValid(10)).isTrue();
+        assertThat(numberSchema.isValid(-10)).isTrue();
+        assertThat(numberSchema.isValid("10")).isFalse();
     }
 
     @Test
     void numberSchemaTestRequired() {
         numberSchema = validator.number().required();
 
-        assertThat(numberSchema.isValid(null)).isEqualTo(false);
-        assertThat(numberSchema.isValid(10)).isEqualTo(true);
-        assertThat(numberSchema.isValid(-10)).isEqualTo(true);
-        assertThat(numberSchema.isValid("10")).isEqualTo(false);
+        assertThat(numberSchema.isValid(null)).isFalse();
+        assertThat(numberSchema.isValid(10)).isTrue();
+        assertThat(numberSchema.isValid(-10)).isTrue();
+        assertThat(numberSchema.isValid("10")).isFalse();
     }
 
     @Test
     void numberSchemaTestPositive() {
         numberSchema = validator.number().positive();
 
-        assertThat(numberSchema.isValid(null)).isEqualTo(true);
-        assertThat(numberSchema.isValid(10)).isEqualTo(true);
-        assertThat(numberSchema.isValid(-10)).isEqualTo(false);
-        assertThat(numberSchema.isValid("10")).isEqualTo(false);
+        assertThat(numberSchema.isValid(null)).isTrue();
+        assertThat(numberSchema.isValid(10)).isTrue();
+        assertThat(numberSchema.isValid(-10)).isFalse();
+        assertThat(numberSchema.isValid("10")).isFalse();
     }
 
     @Test
     void numberSchemaTestRange() {
         numberSchema = validator.number().range(-10, 0);
 
-        assertThat(numberSchema.isValid(null)).isEqualTo(true);
-        assertThat(numberSchema.isValid(10)).isEqualTo(false);
-        assertThat(numberSchema.isValid(-10)).isEqualTo(true);
-        assertThat(numberSchema.isValid("10")).isEqualTo(false);
+        assertThat(numberSchema.isValid(null)).isTrue();
+        assertThat(numberSchema.isValid(10)).isFalse();
+        assertThat(numberSchema.isValid(-10)).isTrue();
+        assertThat(numberSchema.isValid("10")).isFalse();
     }
 
     @Test
     void numberSchemaTestAllMethods() {
         numberSchema = validator.number().required().positive().range(-10, 0);
 
-        assertThat(numberSchema.isValid(null)).isEqualTo(false);
-        assertThat(numberSchema.isValid(10)).isEqualTo(false);
-        assertThat(numberSchema.isValid(-10)).isEqualTo(false);
-        assertThat(numberSchema.isValid("10")).isEqualTo(false);
+        assertThat(numberSchema.isValid(null)).isFalse();
+        assertThat(numberSchema.isValid(10)).isFalse();
+        assertThat(numberSchema.isValid(-10)).isFalse();
+        assertThat(numberSchema.isValid("10")).isFalse();
     }
 
     @Test
     void mapSchemaTestDefault() {
         mapSchema = validator.map();
 
-        assertThat(mapSchema.isValid(null)).isEqualTo(true);
-        assertThat(mapSchema.isValid(new HashMap<>())).isEqualTo(true);
-        assertThat(mapSchema.isValid(MAP_1)).isEqualTo(true);
-        assertThat(mapSchema.isValid(MAP_2)).isEqualTo(true);
+        assertThat(mapSchema.isValid(null)).isTrue();
+        assertThat(mapSchema.isValid(new HashMap<>())).isTrue();
+        assertThat(mapSchema.isValid(MAP_1)).isTrue();
+        assertThat(mapSchema.isValid(MAP_2)).isTrue();
     }
 
     @Test
     void mapSchemaTestRequired() {
         mapSchema = validator.map().required();
 
-        assertThat(mapSchema.isValid(null)).isEqualTo(false);
-        assertThat(mapSchema.isValid(new HashMap<>())).isEqualTo(true);
-        assertThat(mapSchema.isValid(MAP_1)).isEqualTo(true);
-        assertThat(mapSchema.isValid(MAP_2)).isEqualTo(true);
+        assertThat(mapSchema.isValid(null)).isFalse();
+        assertThat(mapSchema.isValid(new HashMap<>())).isTrue();
+        assertThat(mapSchema.isValid(MAP_1)).isTrue();
+        assertThat(mapSchema.isValid(MAP_2)).isTrue();
     }
 
     @Test
     void mapSchemaTestSizeOf() {
         mapSchema = validator.map().sizeOf(2);
 
-        assertThat(mapSchema.isValid(null)).isEqualTo(true);
-        assertThat(mapSchema.isValid(new HashMap<>())).isEqualTo(false);
-        assertThat(mapSchema.isValid(MAP_1)).isEqualTo(false);
-        assertThat(mapSchema.isValid(MAP_2)).isEqualTo(true);
+        assertThat(mapSchema.isValid(null)).isTrue();
+        assertThat(mapSchema.isValid(new HashMap<>())).isFalse();
+        assertThat(mapSchema.isValid(MAP_1)).isFalse();
+        assertThat(mapSchema.isValid(MAP_2)).isTrue();
     }
 
     @Test
     void mapSchemaTestBothMethods() {
         mapSchema = validator.map().required().sizeOf(2);
 
-        assertThat(mapSchema.isValid(null)).isEqualTo(false);
-        assertThat(mapSchema.isValid(new HashMap<>())).isEqualTo(false);
-        assertThat(mapSchema.isValid(MAP_1)).isEqualTo(false);
-        assertThat(mapSchema.isValid(MAP_2)).isEqualTo(true);
+        assertThat(mapSchema.isValid(null)).isFalse();
+        assertThat(mapSchema.isValid(new HashMap<>())).isFalse();
+        assertThat(mapSchema.isValid(MAP_1)).isFalse();
+        assertThat(mapSchema.isValid(MAP_2)).isTrue();
     }
 
     @Test
@@ -209,10 +209,10 @@ public class ValidatorTest {
         Map<String, Object> map3 = new HashMap<>();
         map3.put("name", "Eliza");
         map3.put("age", 24);
-        assertThat(mapSchema.isValid(map3)).isEqualTo(true);
+        assertThat(mapSchema.isValid(map3)).isTrue();
 
         map3.put("name", 24);
         map3.put("age", "Eliza");
-        assertThat(mapSchema.isValid(map3)).isEqualTo(false);
+        assertThat(mapSchema.isValid(map3)).isFalse();
     }
 }
